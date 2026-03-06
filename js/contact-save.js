314ab3098e59b3a6703f2f804d64e85f8219bff7
eodbs0417-byte <eodbs0417@gmail.com>
2026-03-06 10:17:40 +0900

    Update contact-save.js

--- js/contact-save.js
+++ js/contact-save.js
@@ -1,14 +1,10 @@
 function saveContact(name, phone, email = "", department = "", company = "한국도로공사서비스") {
 
-  // 성 / 이름 분리 (한국 이름 기준)
-  const lastName = name.charAt(0);
-  const firstName = name.substring(1);
-
   const vCardData =
 `BEGIN:VCARD
 VERSION:3.0
-N:${lastName};${firstName};;;
-FN:${company} ${name}
+FN:${name}
+N:${name};;;;
 ORG:${company};${department}
 TEL;TYPE=CELL:${phone}
 EMAIL:${email}
@@ -20,6 +16,7 @@ END:VCARD`;
   const link = document.createElement("a");
   link.href = url;
   link.download = name + ".vcf";
+
   document.body.appendChild(link);
   link.click();
 
